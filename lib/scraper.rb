3bd7929e4f506ee0ca89bba1d436ebfced9851e5
Denise Henderson <denise@henderson3.us>
2020-08-18 00:31:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,5 +33,5 @@ class Scraper
   return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
   return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
   return_hash
-  
+
 end
